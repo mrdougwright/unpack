a1a2a34308790b5f2760cc9a1a70e0b6d7e7298b
Doug Wright <mrdougwright@gmail.com>
2018-09-30 11:05:14 -0700

    more mix updates

--- mix.exs
+++ mix.exs
@@ -12,13 +12,12 @@ defmodule Unpack.MixProject do
       # Docs
       name: "Unpack",
       description: "Reach in and unpack any value from a nested map.",
-      licenses: ["MIT"],
-       links: %{"GitHub" => "https://github.com/mrdougwright/unpack"},
       source_url: "https://github.com/mrdougwright/unpack",
       docs: [
         main: "unpack",
         extras: ["README.md"]
-      ]
+      ],
+      package: package(),
     ]
   end
 
@@ -33,6 +32,18 @@ defmodule Unpack.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mrdougwright/unpack"},
+      files: [
+        "lib/unpack.ex",
+        "mix.exs",
+        "README.md"
+      ]
     ]
   end
 end
