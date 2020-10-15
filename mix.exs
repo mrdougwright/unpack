a3a9babfe7cb86e0af96addc0afb4171ad149c58
Doug Wright <mrdougwright@gmail.com>
2020-10-15 09:37:35 -0500

    update Ex Docs

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Unpack.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.19.0", only: :dev, runtime: false}
     ]
   end
 
